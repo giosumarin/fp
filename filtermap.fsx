cb689ead8eebe6978d2c21710f5b8e88b940a506
giosumarin <giosumarin@yahoo.com>
2016-04-09 11:41:37 +0200

    aggiunti filter e fold

---
+++ filtermap.fsx
@@ -0,0 +1,54 @@
+﻿let rec map f l=
+    match l with
+        | []->[]
+        | h::ls->f h::map f ls
+
+       // val map : f:('a -> 'b) -> l:'a list -> 'b list???
+
+map (fun x->x*x) [1 .. 10];
+
+map (fun x->match x%2 with | 0 ->(string)x+", pari" | _ -> (string)x+", dispari") [1 .. 10];;
+
+map (fun (x,y)->"Dott. "+(string)x+" "+(string)y) [ ("Mario", "Rossi") ; ("Anna Maria", "Verdi") ; ("Giuseppe", "Di Gennaro")];;
+
+#r "FsCheck.dll";;
+open FsCheck;;
+
+let prop_map f (ls : int list)=
+    List.map f ls=map f ls;;
+
+Check.Quick prop_map;;//????
+
+let prop_map_pres_len f (ls :int list)=
+    List.length(List.map f ls)=List.length(map f ls);;
+
+Check.Quick prop_map_pres_len;;
+
+let rec filter p l=
+    match l with
+        | []->[]
+        | h::ls when p h->h::filter p ls
+        | h::ls->filter p ls
+
+let mult3 n=
+    filter (fun x->x%3=0) [1 .. n];;
+
+let rec filter1 p l=
+    match l with
+        | []->([],[])
+        | h::ls when p h->
+            let (x,y)=filter1 p ls
+            (h::x,y)
+        | h::ls->
+            let (x,y)=filter1 p ls
+            (x,h::y)
+
+let mult3a n=
+    filter1 (fun x->x%3=0) [1 .. n];;
+
+let divisori n=
+    filter (fun x->n%x=0) [1 .. n];;
+
+let isPrime n=
+    List.length(divisori n)=2;;
+
---
+++ fold.fsx
@@ -0,0 +1,33 @@
+﻿let concat l=List.foldBack (@) l [];;
+concat [[1 .. 5];[6 .. 10]]
+
+let filter p l=List.foldBack(fun x y-> if (p x) then x::y else y) l [];
+filter (fun x->x>5) [1 .. 10];
+
+let rec reduceB f l=
+    match l with
+        | []->failwith"woo"
+        | [x]->x
+        | h::ls->f h (reduceB f ls)
+
+let last l=reduceB (fun x y->y) l
+last [1 .. 10]
+
+#r "FsCheck.dll"
+open FsCheck
+
+let prop_reduce f l=
+    if (List.length l=0) then true else reduceB f l=List.reduceBack f l
+
+Check.Quick prop_reduce
+
+let reduceBF f l=List.foldBack f (l |> List.rev |> List.tail |> List.rev) (l |> List.rev |> List.head)
+
+let last1 l=reduceBF (fun x y->y) l
+last1 [1 .. 10]
+
+
+let prop_reduce1 f l=
+    if (List.length l=0) then true else reduceBF f l=List.reduceBack f l
+
+Check.Quick prop_reduce
