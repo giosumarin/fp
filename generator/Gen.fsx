9831f4f52edf1c9ecedae62e43e16f7318add192
Giosuè Marinò <giosumarin@yahoo.com>
2016-06-07 16:14:44 +0200

    aggiunto appunti da portare all'esame!

--- generator/Gen.fsx
+++ generator/Gen.fsx
@@ -12,25 +12,27 @@ let rec insert x l=
         | [] -> [x]
         | h::tl -> if x < h then x::h::tl else h::(insert x tl);;
 
-
-
 let rec remove x l=
     match l with
         | [] -> []
         | h::tl -> if x=h then (remove x tl) else h::(remove x tl);;
 
+let rec remDup l=
+    match l with
+        | []->[]
+        | h::tl->h::remDup(remove h tl);;
+
+Arb.mapFilter remDup (fun x->List.length x>=2) Arb.from<int list> |> Arb.toGen |> Gen.sample 5 5;; 
 
 
 let genSet genL n m=
-    Arb.mapFilter (List.foldBack (fun x acc->remove x acc) []) (fun x->isOrdered x && List.length x>=m && List.length x<=n) genL;;
+    Arb.mapFilter (id) (fun x->isOrdered x && List.length x>=m && List.length x<=n) genL;;
 
-
-(genSet Arb.from<int list> 3 5 ) |> Arb.toGen |> Gen.sample 50 5;;
+(genSet Arb.from<int list> 3 5 ) |> Arb.toGen |> Gen.sample 10 3;;
 
 let genZip g=
     Arb.mapFilter id (fun (x,y)->List.length x=List.length y) g;;
 
- 
 genZip Arb.from<int list * int list> |> Arb.toGen |> Gen.sample 50 2;;
 
 let propZip x=
@@ -47,19 +49,11 @@ Arb.from<int Tree> |> Arb.toGen |> Gen.sample 50 2;;
 type Color= | Blue | Red | Black;;
 type Block = | Jolly | C of Color;;
 
-//generatore con probabilità 1 di generare joker
-
 let isPositive l=
-    List.foldBack (fun x acc-> (x>=0)&&acc) l true;;
-   // List.forall (f)
-
-let sortDup l=
-    (List.foldBack (fun x acc->x::remove x acc) l l) |> List.sort;; 
-
-sortDup [6;6;1;1;1]
+    List.forall (fun x -> x>0) l;;
 
 let genPos genL=
-    Arb.mapFilter sortDup (fun x->isPositive x && List.length x> 3) genL;;
+    Arb.mapFilter List.sort (fun x->isPositive x && List.length x> 3) genL;;
 
 genPos Arb.from<int list> |> Arb.toGen |> Gen.sample 50 10;;
 
@@ -68,7 +62,7 @@ let freq = Gen.frequency [(6,Gen.map(fun _ -> Jolly) Arb.generate<int>);(4,Gen.m
 Gen.sample 5 10 freq
 
 Arb.generate<Block> |> Gen.sample 5 5
-
+Arb.from<Block> |> Arb.toGen |> Gen.sample 5 5;;
 
 Gen.frequency [(6,Arb.generate<Color>)]
 
